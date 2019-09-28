5a17b909cc76f0b1489996f905af72b7909b9a24
Lakshmi Rajeswara Rao <raje.sarma@gmail.com>
2019-09-28 17:46:59 +0530

    Modified for Zipkin Service

--- zipkin/src/main/java/com/hcl/zipkin/ZipkinApplication.java
+++ zipkin/src/main/java/com/hcl/zipkin/ZipkinApplication.java
@@ -2,9 +2,11 @@ package com.hcl.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableZipkinServer
 public class ZipkinApplication {
 
